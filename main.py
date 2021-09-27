1a02c7ad18c635758cdd8e95f2df988744e81f69
EdwardBrodskiy <49645295+EdwardBrodskiy@users.noreply.github.com>
2021-09-27 19:38:58 +0100

    implemented a yolo rotation approach

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 from tkinter import *
 import cv2
 import numpy as np
-
+from math import cos, sin, pi
+import time
 
 class MasterGUI:
     def __init__(self, root):
         self.width = 600
         self.height = 600
+        image = self.get_image()
+        max_dimension = max(*image.shape[:-1])
+
+        self.border = int(round((2 ** .5 * max_dimension - max_dimension + 1) / 2 + .5, 0))
+        self.size = max_dimension + 2 * self.border
+        self.step = int(round(self.width / self.size, 0))
+
+        self.image = np.zeros((self.size, self.size, 3))
+        self.image[:, :] = image[0, 0]
+        other_border = (self.size - min(*image.shape[:-1])) / 2
+
+        other_border_approx = int(other_border)
+        other_border_extra = int((other_border - other_border_approx) * 2)
+
+        self.image[self.border:-self.border, other_border_approx + other_border_extra: -other_border_approx] = image
+
         self.root = root
 
         self.root.title("Rotating Raster")
@@ -14,16 +31,16 @@ class MasterGUI:
         self.canvas = Canvas(root, width=self.width, height=self.height)
         self.canvas.pack()
 
-        self.image = self.get_image()
+        while True:
+            self.run_rotation()
 
-        self.draw()
 
     def get_image(self):
         width = 13
         height = 10
 
         img = cv2.imread(f'space-invader.jpg')
-        print(img.shape)
+
         x_step = img.shape[1] / width
         y_step = img.shape[0] / height
 
@@ -34,12 +51,37 @@ class MasterGUI:
 
         return reduced_img
 
-    def draw(self):
-        step = 30
-        for x in range(self.image.shape[0]):
-            for y in range(self.image.shape[1]):
-                color = '#' + ''.join(list(map(lambda px: hex(int(px))[2:], self.image[x, y, :])))
-                self.canvas.create_rectangle(x * step, y * step, x * step + step, y * step + step, fill=color, outline='')
+    def run_rotation(self):
+        start = time.time()
+        total_time = 10
+        while time.time() - start < total_time:
+            self.draw(self.rotate_yolo(self.image, (time.time() - start) / total_time * 2 * pi))
+
+    def draw(self, image):
+        self.canvas.delete('all')
+        for x in range(image.shape[0]):
+            for y in range(image.shape[1]):
+                color = '#' + ''.join(list(map(lambda px: hex(int(px))[2:], image[x, y, :])))
+                sx, sy = x * self.step, y * self.step
+                self.canvas.create_rectangle(sx, sy, sx + self.step, sy + self.step, fill=color, outline='')
+        self.root.update_idletasks()
+        self.root.update()
+
+    def rotate_yolo(self, image, radians):
+        rotation_mat = np.array([[cos(radians), -sin(radians)],
+                                 [sin(radians), cos(radians)]])
+        rotated_image = np.zeros((self.size, self.size, 3))
+        rotated_image[:, :] = image[0, 0]
+
+        center_shift = self.size / 2
+        for i in range(self.border, self.size - self.border):
+            for j in range(self.border, self.size - self.border):
+                old_location = np.array([[i], [j]])
+                old_location = old_location - center_shift
+                new_location = np.matmul(rotation_mat, old_location)
+                new_location = np.round(new_location + center_shift)
+                rotated_image[int(new_location[0, 0]), int(new_location[1, 0])] = image[i, j]
+        return rotated_image
 
 
 app = Tk()
